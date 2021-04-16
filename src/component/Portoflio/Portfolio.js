3579828b3ab1dcfd6b862e65525db67b91b5d30a
Stainley Lebron <stainley.lebron@gmail.com>
2021-04-16 19:25:05 -0400

    lower threshold

--- src/component/Portoflio/Portfolio.js
+++ src/component/Portoflio/Portfolio.js
@@ -1,11 +1,15 @@
 import React, {Fragment} from "react";
 
 const Portfolio = () => {
+    let name;
+
     return (
         <Fragment>
+            <h1>Portfolio</h1>
             <h1>Portfolio</h1>
         </Fragment>
     );
 };
 
 export default Portfolio;
+
