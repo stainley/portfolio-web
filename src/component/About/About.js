49dc1b77a82d63aedd838e5976c4e7e31366b1eb
Stainley Lebron <stainley.lebron@gmail.com>
2021-04-17 09:42:44 -0400

    Multi enviroment

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const About = () => {
+const About = ({title, description}) => {
+    let data = null;
+
     return(
         <div>
-            About Me
+            About Me {description === null? 'Cannot be null': <h1>{description}</h1>};
         </div>
     );
 };
