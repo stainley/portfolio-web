8985d91e53a905d77b9828cda06bdcba436c2500
Stainley Lebron <stainley.lebron@gmail.com>
2021-04-21 11:35:24 -0400

    fix

--- jenkins/scripts/deploy-for-dev.sh
+++ jenkins/scripts/deploy-for-dev.sh
@@ -9,7 +9,7 @@ docker container ls -q --filter "name=$APP_NAME" | grep -q . && docker container
 
 echo "Building Docker Image $APP_NAME-dev:$PORTFOLIO_VERSION"
 #docker  build -t stainley/portfolio-web-dev:$PORTFOLIO_VERSION -f Dockerfile.dev .
-docker  build --build-arg "$PORTFOLIO_VERSION" -t "stainley/$APP_NAME-dev:$PORTFOLIO_VERSION stainley/$APP_NAME-dev:latest" -f Dockerfile.dev .
+docker  build --build-arg "$PORTFOLIO_VERSION" -t "stainley/$APP_NAME-dev:$PORTFOLIO_VERSION" -t "stainley/$APP_NAME-dev:latest" -f Dockerfile.dev .
 #docker  build --build-arg "$PORTFOLIO_VERSION" -t stainley/portfolio-web-dev:"$PORTFOLIO_VERSION" -f Dockerfile.dev .
 #echo 'building a tag'
 #docker tag portfolio-web-dev stainley/portfolio-web:$PORTFOLIO_VERSION
