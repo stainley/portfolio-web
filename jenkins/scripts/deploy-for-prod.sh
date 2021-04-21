2af869ae00130c23a85930d62a6c79a66c858f5a
Stainley Lebron <saiyamans@gmail.com>
2021-04-21 10:16:35 -0400

    Development (#9)
    * dockerhub
    
    * Fix transfer file using SCP
    
    * fix path to the file
    
    * docker tag with latest version

--- jenkins/scripts/deploy-for-prod.sh
+++ jenkins/scripts/deploy-for-prod.sh
@@ -9,12 +9,12 @@ docker container ls -q --filter "name=$APP_NAME" | grep -q . && docker container
 
 echo "Building Docker Image $PORTFOLIO_VERSION"
 #docker  build -t stainley/portfolio-web-dev:$PORTFOLIO_VERSION -f Dockerfile.dev .
-docker  build --build-arg "$PORTFOLIO_VERSION" -t "stainley/$APP_NAME:$PORTFOLIO_VERSION" -f Dockerfile.prod .
+docker  build --build-arg "$PORTFOLIO_VERSION" -t "stainley/$APP_NAME:$PORTFOLIO_VERSION stainley/$APP_NAME:latest" -f Dockerfile.prod .
 #echo 'building a tag'
 
 echo "Removing intermediate Image for $APP_NAME:$PORTFOLIO_VERSION"
 docker image prune --force --filter label=stage=builder --filter label=build="$PORTFOLIO_VERSION"
 
 echo "Uploading image to Docker Hub: stainley/$APP_NAME:$PORTFOLIO_VERSION"
-docker push "stainley/$APP_NAME:$PORTFOLIO_VERSION"
+docker push "stainley/$APP_NAME:$PORTFOLIO_VERSION stainley/$APP_NAME:latest"
 echo "----- Docker Image uploaded to Docker Hub successfully ----"
