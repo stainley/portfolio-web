3c84230b273e38fb202b868774f0c9114d283620
Stainley Lebron <stainley.lebron@gmail.com>
2021-04-22 10:44:16 -0400

    Remove local registry

--- jenkins/scripts/deploy-for-dev.sh
+++ jenkins/scripts/deploy-for-dev.sh
@@ -9,12 +9,12 @@ docker container ls -q --filter "name=$APP_NAME" | grep -q . && docker container
 
 echo "Building Docker Image $APP_NAME-dev:$PORTFOLIO_VERSION"
 #docker  build -t stainley/portfolio-web-dev:$PORTFOLIO_VERSION -f Dockerfile.dev .
-docker  build --build-arg "$PORTFOLIO_VERSION" -t "kubernetes-master:5000/$APP_NAME-dev:$PORTFOLIO_VERSION" -t "kubernetes-master:5000/$APP_NAME-dev:latest" -f Dockerfile.dev .
-#docker  build --build-arg "$PORTFOLIO_VERSION" -t stainley/portfolio-web-dev:"$PORTFOLIO_VERSION" -f Dockerfile.dev .
+#docker  build --build-arg "$PORTFOLIO_VERSION" -t "kubernetes-master:5000/$APP_NAME-dev:$PORTFOLIO_VERSION" -t "kubernetes-master:5000/$APP_NAME-dev:latest" -f Dockerfile.dev .
+docker  build --build-arg "$PORTFOLIO_VERSION" -t "stainley/$APP_NAME-dev:$PORTFOLIO_VERSION" -t "stainley/$APP_NAME-dev:latest" -f Dockerfile.dev .
 #echo 'building a tag'
 #docker tag portfolio-web-dev stainley/portfolio-web:$PORTFOLIO_VERSION
 echo "Removing intermediate Image for $APP_NAME:$PORTFOLIO_VERSION"
 docker image prune --force --filter label=stage=builder --filter label=build="$PORTFOLIO_VERSION"
 
-echo "Uploading image $REACT_APP_NAME-dev to Local Docker Registry"
-docker push "kubernetes-master:5000/$REACT_APP_NAME-dev"
+#echo "Uploading image $REACT_APP_NAME-dev to Local Docker Registry"
+#docker push "kubernetes-master:5000/$REACT_APP_NAME-dev"
